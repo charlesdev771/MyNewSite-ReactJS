836ec8ffe162081af1592e74484d72df83ccb419
charlesdev771 <charlesdev0721@protonmail.com>
2022-09-22 05:36:50 -0300

    Centralization

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,49 +6,58 @@ import styles from "./css/Projects.css";
 export default function Projects()
 {
     return (
-
-        <div id="PROJECTS" className="PROJECTS">
-            <div id="projects" className="projects">
-
-                <div>
-
-                    <h1 className="text-center text-dark col-12 mt-5 mb-5 pro">
-                        Experiências:
-                    </h1>
-
-                </div>
-
-                <table class="table table-dark w-100 p-3 pro">
-                    <tbody>
-                        <tr>
-                            <td>Site: <br />http://cdev7.epizy.com/ (ON) <br />Stack: HTML5, CSS3, Bootstrap JS, PHP7, MYSQL</td>
-                            </tr>
-                            <tr>
-                            <td>Site: <br /> http://fekrn.epizy.com/index.php (OFF) <br /> Stack: HTML5, CSS3, JS, PHP7, MYSQL</td>
-                            </tr>
-                            <tr>
-                            <td>Prioriser: https://github.com/charlesdev771/Project-prioriser (ON) <br /> Stack: HTML5, CSS3, JS, PHP7, MYSQL, Java, React Native</td>
-                            </tr>
-                            <tr>
-                            <td>Site: <br /> http://sitesamuray.epizy.com/ (OFF) <br /> Stack: HTML5, CSS3, JS, PHP7, MYSQL</td>
-                            </tr>
-                            <tr>
-                            <td>Site: <br /> http://maisfitness.epizy.com/home/ (OFF) <br /> Stack: HTML5, CSS3, Bootstrap, JS, PHP7, MYSQL</td>
-                            </tr>
-                            <tr>
-                            <td>Forum: indisponível (OFF) <br /> Stack: HTML5, CSS3, JS, Python, Django, MYSQL</td>
-                            </tr>
-                            <tr>
-                            <td>Blog: indisponível (OFF) <br /> Stack: HTML5, CSS3, JS, Python, Django, MYSQL</td>
-                            </tr>
-                            <tr>
-                            <td>Repositórios: https://github.com/charlesdev771 (ON) <br /> Stack: HTML5, CSS3, JS, PHP7, MYSQL, Python, Django, Flask, React, React Native, Vue, etc...</td>
-                            </tr>
-                        </tbody>
-                        </table>
-                        <br /> 
+        <div>
+            <div>
+                <h3 className="text-center text-dark col-12 mt-5 mb-5 pro display-4 textProject">
+                    Experiências:
+                </h3>
             </div>
-        </div>
+            <div class="card container-fluid bg-dark text-light mt-5 cardProject">
+                <div class="card-body ">
+                    <p class="card-text">Site: <br />http://cdev7.epizy.com/ (ON) <br />Stack: HTML5, CSS3, Bootstrap JS, PHP7, MYSQL</p>
+                </div>
+            </div>
+            <div class="card container-fluid bg-dark text-light cardProject">
+                <div class="card-body">
+                    <p class="card-text">http://fekrn.epizy.com/index.php (OFF) <br /> Stack: HTML5, CSS3, JS, PHP7, MYSQL</p>
+                </div>
+            </div>
+            <div class="card container-fluid bg-dark text-light cardProject">
+                <div class="card-body">
+                    <p class="card-text">Prioriser: https://github.com/charlesdev771/Project-prioriser (ON) <br /> Stack: HTML5, CSS3, JS, PHP7, MYSQL, Java, React Native</p>
+                </div>
+            </div>
+            <div class="card container-fluid bg-dark text-light cardProject">
+                <div class="card-body">
+                    <p class="card-text">Site: <br /> http://sitesamuray.epizy.com/ (OFF) <br /> Stack: HTML5, CSS3, JS, PHP7, MYSQL</p>
+                </div>
+            </div>
+            <div class="card container-fluid bg-dark text-light cardProject">
+                <div class="card-body">
+                    <p class="card-text">Site: <br /> http://maisfitness.epizy.com/home/ (OFF) <br /> Stack: HTML5, CSS3, Bootstrap, JS, PHP7, MYSQL</p>
+                </div>
+            </div>
+            <div class="card container-fluid bg-dark text-light cardProject">
+                <div class="card-body">
+                <p class="card-text">Forum: indisponível (OFF) <br /> Stack: HTML5, CSS3, JS, Python, Django, MYSQL</p>
+                </div>
+            </div>
+            <div class="card container-fluid bg-dark text-light cardProject">
+                <div class="card-body">
+                <p class="card-text">Blog: indisponível (OFF) <br />Stack: HTML5, CSS3, JS, Python, Django, MYSQL</p>
+                </div>
+            </div>
+            <div class="card container-fluid bg-dark text-light cardProject">
+                <div class="card-body">
+                <p class="card-text">Repositórios: https://github.com/charlesdev771 (ON) <br /> Stack: HTML5, CSS3, JS, PHP7, MYSQL, Python, Django, Flask, React, React Native, Vue, etc...</p>
+                </div>
+            </div>
+            <br />
+            <br />
+            </div>
+
+
+
 
     )
 }
