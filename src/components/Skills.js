4f72288043984266b06d4b4536d27c357460456a
charlesdev771 <charles077211@gmail.com>
2023-07-11 20:19:12 -0300

    Add test

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,12 +13,17 @@ export default function Skills()
         <h1 className="text-center mt-5 mb-3">Principais habilidades</h1>
 
             <div class="card-body">
-                <p class="card-text">WEB: fornt-end com HTML5, CSS3, JS, React e VUE. Back-end com Flask, Django, NODEJS e PHP.</p>
+                <p class="card-text">WEB: front-end com HTML5, CSS3, JS, React e VUE. Back-end com Flask, Django, NODEJS e PHP.</p>
             </div>
         </div>
         <div class="card container-fluid bg-dark text-light">
             <div class="card-body">
-                <p class="card-text">S.I ofensiva: Linux, Windows, Metasploit, Maltego, Fierce, ChameleonPY, python, shell script, assembly e etc.</p>
+                <p class="card-text">Mobile: react-native, flutter.</p>
+            </div>
+        </div>
+        <div class="card container-fluid bg-dark text-light">
+            <div class="card-body">
+                <p class="card-text">S.i ofensiva: Linux, Windows, Metasploit, Maltego, Fierce, ChameleonPY, python, shell script, assembly e etc.</p>
             </div>
         </div>
         
