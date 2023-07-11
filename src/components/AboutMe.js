d8fc8171ccd3fb20ffafae62ebac14d1fec76e65
charlesdev771 <charles077211@gmail.com>
2023-07-11 20:25:52 -0300

    final 2.0

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -17,12 +17,12 @@ export default function AboutMe()
                    Sou estudante de programação e segurança da informação. Atualmente estou 
                    focado na área web e pentest. Pretendo em breve começar a estudar sobre 
                    blockchain e inteligencia artificial. Atualmente estudo no IFRN campus SGA 
-                   na turma de informática para internet e estou cursando o terceiro ano. 
+                   na turma de informática para internet e estou cursando o quarto ano. 
                    Trabalho hoje com HTML5, CSS3, Javascript, react, vueJs, nodeJS, python, 
                    flask, django, c, shell script, linux, windows,  e pentest de forma geral. 
                    Já estudei outras várias linguagens como:  Assembly, cobol, c++, java, dart, 
-                   ruby, R e algumas outras. Amo tecnologia e tudo que está relacionado a ela. Além disso sou o criador de algumas ferramentas 
-                   de software livre. Entre elas: chameleonPY, PYBash, PID, KillerQueen e algumas outras. Amo escrever, jogar, estudar e ler. aqui está o link onde ficará meus livros: 
+                   ruby, R e algumas outras. Amo tecnologia e tudo que está relacionado a ela. Assim também com ciências exatas. Além disso sou o criador de algumas ferramentas 
+                   de software livre. Entre elas: chameleonPY, PYBash, PID, KillerQueen e algumas outras. Amo escrever, jogar, estudar e ler. aqui está o link onde ficará meus livros: https://drive.google.com/drive/folders/1qS-dJox6-9CmChU01NH4kIvEaSHxmvKH?usp=sharing 
                 </p>
             </div>
         </div>
